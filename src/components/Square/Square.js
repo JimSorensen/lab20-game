135ce5b856a2cef827c54cfb0693131523a5e016
JimSorensen <56436477+JimSorensen@users.noreply.github.com>
2022-05-24 15:50:32 +0200

    Opgavenfærdig

---
+++ src/components/Square/Square.js
@@ -0,0 +1,10 @@
+import '../../index.css';
+
+const Square = (props) => {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  };
+  export default Square
